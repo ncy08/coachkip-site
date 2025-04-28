d314ea955fc313be08788437e082784f72731c32
Papitkz <johnfritzizar35@gmail.com>
2025-04-28 23:11:24 +0800

    revised sitemap dynamic modified  date

--- src/components/Sitemap.vue
+++ src/components/Sitemap.vue
@@ -1,6 +1,8 @@
 <template>
-    <div v-html="sitemap"></div>
-  </template>
+    <div v-html="sitemap">
+
+    </div>
+</template>
   
   <script>
   export default {
@@ -15,13 +17,13 @@
     methods: {
       generateSitemap() {
         const urls = [
-          { loc: 'https://kip.coach/', lastmod: '2025-04-28', changefreq: 'monthly', priority: '1.0' },
-          { loc: 'https://kip.coach/about', lastmod: '2025-04-28', changefreq: 'monthly', priority: '0.8' },
+          { loc: 'https://kip.coach/', lastmod: new Date().toISOString().split('T')[0], changefreq: 'monthly', priority: '1.0' },
+          { loc: 'https://kip.coach/about', lastmod: new Date().toISOString().split('T')[0], changefreq: 'monthly', priority: '0.8' },
           // Add more URLs as needed
         ];
   
         let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
-        xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1">\n`;
+        xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.0">\n`;
   
         urls.forEach(url => {
           xml += `  <url>\n`;
