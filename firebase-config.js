484fe863bd12bad0d9fc6f5720875952762782ce
John Fritz Izar <johnfritzizar35@gmail.com>
2025-05-09 15:31:08 +0800

    Add files via upload
    revised

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,15 @@
 import { initializeApp } from "firebase/app";
-   import { getAnalytics } from "firebase/analytics";
-   // Your web app's Firebase configuration
-   const firebaseConfig = {
-     apiKey: "AIzaSyBkRsLDDkITPdFKP9pHO1YhEiD4QZWr1Vw",
-     authDomain: "coach-kip-d6793.firebaseapp.com",
-     projectId: "coach-kip-d6793",
-     storageBucket: "coach-kip-d6793.firebasestorage.app",
-     messagingSenderId: "170697876230",
-     appId: "1:170697876230:web:bf79a8508d1f9f73523487",
-     measurementId: "G-QMD5MYZ5Y4"
-   };
-   // Initialize Firebase
-   const app = initializeApp(firebaseConfig);
-   const analytics = getAnalytics(app);
-   
+import { getAnalytics } from "firebase/analytics";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkRsLDDkITPdFKP9pHO1YhEiD4QZWr1Vw",
+  authDomain: "coach-kip-d6793.firebaseapp.com",
+  projectId: "coach-kip-d6793",
+  storageBucket: "coach-kip-d6793.appspot.com", // Corrected line
+  messagingSenderId: "170697876230",
+  appId: "1:170697876230:web:bf79a8508d1f9f73523487",
+  measurementId: "G-QMD5MYZ5Y4"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app); // Optional: Remove if not using analytics
