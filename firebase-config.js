85aa2b26be6215cd54bd239138d2341e95d271b7
John Fritz Izar <johnfritzizar35@gmail.com>
2025-05-10 10:28:03 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,21 @@
+// src/firebase-config.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// Your web app's Firebase configuration
+
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyBkRsLDDkITPdFKP9pHO1YhEiD4QZWr1Vw",
   authDomain: "coach-kip-d6793.firebaseapp.com",
   projectId: "coach-kip-d6793",
-  storageBucket: "coach-kip-d6793.appspot.com", // Corrected line
+  storageBucket: "coach-kip-d6793.appspot.com",
   messagingSenderId: "170697876230",
   appId: "1:170697876230:web:bf79a8508d1f9f73523487",
   measurementId: "G-QMD5MYZ5Y4"
 };
-// Initialize Firebase
+
+// Initialize Firebase and Analytics
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); // Optional: Remove if not using analytics
+const analytics = getAnalytics(app);
+
+export { app, analytics };
+  
